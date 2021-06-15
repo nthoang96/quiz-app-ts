2f3cc86cfb764f39f93e10f3cd6204e1699f8503
nthoang96 <hoanglion.it@gmail.com>
2021-06-15 23:36:09 +0700

    UI for Quizz, set up call API, show question, change question

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { loginSlice } from "../containers/Login/loginSlice";
 import { snackbarSlice } from "../containers/Snackbar/snackbarSlice";
+import { quizzSlice } from "../pages/Quizz/quizzSlice";
 
 const store = configureStore({
   reducer: {
     login: loginSlice.reducer,
     snackbar: snackbarSlice.reducer,
+    quizz: quizzSlice.reducer,
   },
 });
 
@@ -14,5 +16,9 @@ type RootState = ReturnType<typeof store.getState>;
 export const selectUseLogin = (state: RootState) => state.login.useLogin;
 export const selectDisplayName = (state: RootState) => state.login.displayName;
 export const selectSnackbar = (state: RootState) => state.snackbar;
+export const selectQuestion = (state: RootState) => state.quizz.questions;
+export const selectAnswer = (state: RootState) => state.quizz.answers;
+export const selectCurrentQuestion = (state: RootState) =>
+  state.quizz.currentQuestion;
 
 export default store;
